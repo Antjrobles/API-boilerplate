b9347dd3242297db4032d20a929fb21f5322b6d7
Antonio Robles <antjrobles@gmail.com>
2023-06-08 15:21:32 +0200

    3 endpoints added

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ app.listen(port, localhost, () => {
 });                              // Arrow function to tell the app use port and localhost as parameters and print in the console the information using template string.      
 
 
+// HOME
+app.get('/', (req, res) => {
+  res.send(`<h1>"Welcome Home!"</h1>`)   //Home. Display a message
+})
+
+// ENDPOINTS
+app.get('/api/codepens', (req, res) => {     
+  const links = {
+    grid: 'https://codepen.io/Antjrobles/pen/qBJwjWV',
+    crypto_marquee: "https://codepen.io/Antjrobles/pen/OJBzMEB"
+  };
+  res.send(links);    // Display the links on the browser
+});
+
+app.get('/api/data', (req, res) => {
+  
+    const data = {
+      name: 'John Doe',
+      age: 25,
+      city: 'San Diego',
+      state: 'California',
+      country: 'United States',
+      hobbies: ['coding', 'reading', 'gaming', 'hiking', 'traveling'],
+    }
+    res.send(data);   // Display the data on the browser
+})
 
 
 
