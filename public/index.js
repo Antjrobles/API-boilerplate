1258d1b2d93c244ed3ee3f7b7f1af9821710dee2
Antjrobles <antjrobles@gmail.com>
2023-09-28 13:24:54 +0200

    test for mpurebas

--- public/index.js
+++ public/index.js
@@ -1,31 +1,4 @@
 
-
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll(".data, .countries, .codes");
-
-  links.forEach(link => {
-    link.addEventListener("click", async (e) => {
-      e.preventDefault();
-
-      const url = e.target.getAttribute("data-url"); // Obtener la URL del atributo data-url
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        const jsonContainer = document.getElementById("json-container");
-        jsonContainer.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-      } catch (error) {
-        console.error("Error al obtener JSON:", error);
-      }
-    });
-  });
-});
-
-
-
-
-
-
-
 // THIS  WORKS WITH "DATA". IT  EXTRACT  THE "CODES"
 
 
