829b6d47e5ec9f14609d3e11dade0b5ea2f351f0
Antjrobles <antjrobles@gmail.com>
2023-09-28 16:04:10 +0200

    button functionality

--- public/index.js
+++ public/index.js
@@ -1,7 +1,24 @@
 
+//const data = document.querySelectorAll(".data");
+//const countries = document.querySelectorAll(".countries");
+//const countryCodes = document.querySelectorAll(".codes");
+const btn = document.getElementById("btn");
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll(".data, .countries, .codes");
+
+
+  btn.addEventListener('click', (e) => {
+      (e.preventDefault());
+      window.location.href = '/';
+  })
+
+
+
 
   links.forEach(link => {
     link.addEventListener("click", async (e) => {
@@ -13,9 +30,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const data = await response.json();
         const jsonContainer = document.getElementById("json-container");
         jsonContainer.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+
+        btn.style.display = "block";
       } catch (error) {
         console.error("Error al obtener JSON:", error);
       }
     });
+
   });
 });
